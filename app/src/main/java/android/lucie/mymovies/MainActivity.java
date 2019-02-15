7bb0420d64a5c80a215a60409d53c33671048223
lulb8 <bonneville@et.esiea.fr>
2019-02-15 21:45:10 +0100

    Appel API rest

--- app/src/main/java/android/lucie/mymovies/MainActivity.java
+++ app/src/main/java/android/lucie/mymovies/MainActivity.java
@@ -1,32 +1,52 @@
 package android.lucie.mymovies;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import android.lucie.mymovies.model.Movie;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends Activity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
+    private MainController controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView = findViewById(R.id.my_recycler_view);
+
+        controller = new MainController(this);
+        controller.onStart();
+    }
+
+    public void showList(List<Movie> input){
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            input.add("Test" + i);
-        }
+        // define an adapter
         mAdapter = new MovieAdapter(input);
         recyclerView.setAdapter(mAdapter);
     }
--- app/src/main/java/android/lucie/mymovies/MovieAdapter.java
+++ app/src/main/java/android/lucie/mymovies/MovieAdapter.java
@@ -1,82 +1,87 @@
 package android.lucie.mymovies;
 
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.lucie.mymovies.model.Movie;
 
 import java.util.List;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
-    private List<String> values;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.CelluleJava> {
+    private List<Movie> listValues;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class CelluleJava extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public TextView txtHeader;
         public TextView txtFooter;
+        public ImageView image;
         public View layout;
 
-        public ViewHolder(View v) {
+        //Constructeur
+        public CelluleJava(View v) {
             super(v);
             layout = v;
             txtHeader = (TextView) v.findViewById(R.id.title);
             txtFooter = (TextView) v.findViewById(R.id.synopsis);
+            image = v.findViewById(R.id.poster);
         }
     }
 
-    public void add(int position, String item) {
-        values.add(position, item);
+    public void add(int position, Movie item) {
+        listValues.add(position, item);
         notifyItemInserted(position);
     }
 
     public void remove(int position) {
-        values.remove(position);
+        listValues.remove(position);
         notifyItemRemoved(position);
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MovieAdapter(List<String> myDataset) {
-        values = myDataset;
+    public MovieAdapter(List<Movie> listValues) {
+        this.listValues = listValues;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                      int viewType) {
+    public CelluleJava onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        LayoutInflater inflater = LayoutInflater.from(
-                parent.getContext());
-        View v =
-                inflater.inflate(R.layout.row_movie, parent, false);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.row_movie, parent, false);
         // set the view's size, margins, paddings and layout parameters
-        ViewHolder vh = new ViewHolder(v);
+        CelluleJava vh = new CelluleJava(v);
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
+    public void onBindViewHolder(CelluleJava holder, final int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        final String name = values.get(position);
+        Movie currentMovie = listValues.get(position);
+        final String name = currentMovie.getName();   //TODO faire pour les autres
         holder.txtHeader.setText(name);
         holder.txtHeader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                remove(position);
+                remove(position);   //TODO afficher le detail
             }
         });
 
-        holder.txtFooter.setText("Footer: " + name);
+        holder.txtFooter.setText("Footer: " + name); //TODO mettre la desciption
     }
 
     @Override
     public int getItemCount() {
-        return values.size();
+        return listValues.size();
     }
 
 }
