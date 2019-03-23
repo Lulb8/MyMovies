2eae5d589691cd9466324b5079ae34049394f755
lulb8 <bonneville@et.esiea.fr>
2019-03-23 18:45:13 +0100

    Add Crashlytics

--- app/src/main/java/android/lucie/mymovies/view/MainActivity.java
+++ app/src/main/java/android/lucie/mymovies/view/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 import com.google.gson.Gson;
 
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.my_recycler_view);
 
